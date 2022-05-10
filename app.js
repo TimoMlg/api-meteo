14669f84c9c8c683abbec8b1fa690da8c3f9d956
TimoMlg <52750239+TimoMlg@users.noreply.github.com>
2022-05-10 12:42:44 +0200

    Step-2-complete

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ function btnGetMethod() {
     .then(data => {
         lat = data.results[0].geometry.lat;
         lon = data.results[0].geometry.lng;
-        getDay(data);
         callbackGetOpenCage(lat, lon);
     })
     .catch(err => console.log(err))
@@ -24,8 +23,7 @@ function btnGetMethod() {
 
 const callbackGetOpenCage = function (lat, lon) {
     const API_KEY = "185f5c86e1d85eed7428b5ba6701f372";
-    const url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=" + API_KEY;
-    //const url = "https://api.openweathermap.org/data/2.5/forecast/daily?lat=" + lat +"&lon=" + lon + "&cnt=" + 5 + "&appid=" + API_KEY;
+    const url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY;
     
     //fonction get et callback
     $.get(url, callbackGetSuccess).done()
@@ -34,54 +32,68 @@ const callbackGetOpenCage = function (lat, lon) {
     })
 }
 
-function getDay(data) {
+// function getDay(data) {
 
-    const weather_day = document.getElementById("day");
-    weather_day.innerHTML = data.timestamp.created_http;
-    console.log(weather_day.innerHTML);
+//     const weather_day = document.getElementById("day");
+//     weather_day.innerHTML = data.timestamp.created_http;
+//     console.log(weather_day.innerHTML);
 
-    switch (true) {
-        case weather_day.innerHTML.includes("Mon"):
-            weather_day.innerHTML = "Monday"
-            break;
-        case weather_day.innerHTML.includes("Tue"):
-            weather_day.innerHTML = "Tuesday"
-            break;
-        case weather_day.innerHTML.includes("Wed"):
-            weather_day.innerHTML = "Wednesday"
-            break;
-        case weather_day.innerHTML.includes("Thu"):
-            weather_day.innerHTML = "Thursday"
-            break;
-        case weather_day.innerHTML.includes("Sat"):
-            weather_day.innerHTML = "Saturday"
-            break;
-        case weather_day.innerHTML.includes("Sun"):
-            weather_day.innerHTML = "Sunday"
-            break;
-    }
-}
+//     switch (true) {
+//         case weather_day.innerHTML.includes("Mon"):
+//             weather_day.innerHTML = "Monday"
+//             break;
+//         case weather_day.innerHTML.includes("Tue"):
+//             weather_day.innerHTML = "Tuesday"
+//             break;
+//         case weather_day.innerHTML.includes("Wed"):
+//             weather_day.innerHTML = "Wednesday"
+//             break;
+//         case weather_day.innerHTML.includes("Thu"):
+//             weather_day.innerHTML = "Thursday"
+//             break;
+//         case weather_day.innerHTML.includes("Sat"):
+//             weather_day.innerHTML = "Saturday"
+//             break;
+//         case weather_day.innerHTML.includes("Sun"):
+//             weather_day.innerHTML = "Sunday"
+//             break;
+//     }
+// }
 
 const callbackGetSuccess = function (data) {
-    const img = document.getElementById("img-weather");
-    img.style.display = "block";
+    
+    for (let i = 0; i < data.daily.length-3; i++) {
 
-    switch (true) {
-        case data.current.clouds == 0:
+        const unixTimeStamp = data.daily[i].dt;
+        const dateObject = new Date(unixTimeStamp*1000)
+        const weekDay = dateObject.toLocaleString("en-US", {weekday: "long"}) //2019-12-9 10:30:15
+        const dayH3 = document.createElement("h3");
+        dayH3.innerHTML = weekDay;
+
+        const img = document.createElement('img')
+        img.style.width = "10%";
+        img.style.margin = "20px";
+    
+        switch (true) {
+        case data.daily[i].clouds == 0:
             img.src = "./weathers/sun.svg";
             break;
-        case data.current.clouds > 0 && data.clouds.all <= 50:
+        case data.daily[i].clouds > 0 && data.daily[i].clouds <= 50:
             img.src = "./weathers/cloudy.svg";
             break;
-        case data.current.clouds > 50:
+        case data.daily[i].clouds > 50:
             img.src = "./weathers/clouds.svg";
             break;
-        case data.current.snow > 0:
+        case data.daily[i].snow > 0:
             img.src = "./weathers/snow.svg";
             break;
         default:
             img.src = "./weathers/rain.svg";
             break;
     }
-    console.log(data);
+        document.getElementById("day").appendChild(dayH3);
+        document.getElementById("img_result").appendChild(img);
+        console.log(data);
+    }
+    
 }
