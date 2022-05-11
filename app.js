7a8923555b32024430963d0d0cf347ff43ea6167
TimoMlg <52750239+TimoMlg@users.noreply.github.com>
2022-05-11 16:03:57 +0200

    Feature-nuit-jour-fin-de-projet

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+
 function btnGetMethod() {
     const ville = document.getElementById("input-city").value;     //Récupération de la ville depuis la saisie utilisateur
     var lat; //latitude localisation
     var lon; //longitude localisation
+    var current_hour; //heure actuelle de la ville
     const API_KEY = "7218b7f9356a4f16b14e165aa183d933"; //Api key opencage
     let URL = 'https://api.opencagedata.com/geocode/v1/json?q=' + ville + '&key=' + API_KEY + '&language=fr&pretty=1'
 
@@ -24,7 +26,7 @@ function btnGetMethod() {
 //Appel de l'api openweather en passant la latitude et longitude en paramètres. paramètres à récupérer au niveau de la data extraite de l'api openCage
 const callApiOpenWeather = function (lat, lon) {
     const API_KEY = "185f5c86e1d85eed7428b5ba6701f372";
-    const url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY;
+    const url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY +"&units=metric";
     
     //appel de l'api avec la methode get et appel de la fonction callBackSuccess en cas de succes, sinon error
     $.get(url, callbackGetSuccess).done()
@@ -36,8 +38,12 @@ const callApiOpenWeather = function (lat, lon) {
 TODO: Terminer le sunrise et sunset (bonus)
 */
 const callApiSunriseSunset = function(lat, lon) {
-    const URL = "https://api.sunrise-sunset.org/json?lat=" +  lat + "&lng=" + lon;
-    fetch(URL) // on utilise la methode fetch, qui est asynchrone et qui existe par défaut dans le navigateur (on aurait aussi pu utiliser la librairie axios par exemple)
+
+    const API_KEY = "185f5c86e1d85eed7428b5ba6701f372";
+    const urlOpenWeather = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY +"&units=metric";
+    const URLSunrise = "https://api.sunrise-sunset.org/json?lat=" +  lat + "&lng=" + lon;
+    
+    fetch(URLSunrise) // on utilise la methode fetch, qui est asynchrone et qui existe par défaut dans le navigateur (on aurait aussi pu utiliser la librairie axios par exemple)
     // on utilise la méthode then() (NB: on pourrait aussi utiliser la syntaxe async/await)
     .then(response => { 
     if (response.status == 200) { // on vérifie que l'appel à l'API a fonctionné
@@ -45,8 +51,32 @@ const callApiSunriseSunset = function(lat, lon) {
     }
     else console.log(`Erreur du call de l'api sunrise`);
     })
-    .then(data => {
-        console.log("donnees api sunrise : " + data.results.sunrise);            
+    .then(dataSunrise => {
+        $.get(urlOpenWeather, function (dataOpenWeather, status) {
+            
+            const unixTimeStamp = dataOpenWeather.current.dt; //get timestamp in data
+            const dateObject = new Date(unixTimeStamp*1000) //convert to millisecond
+            var timeOfCity = dateObject.toLocaleString("en-US", {hour: "numeric", minute: "numeric", second: "numeric"}) // HH:mm:ss AM
+
+            const sunriseTime = dataSunrise.results.sunrise;
+            const sunsetTime = dataSunrise.results.sunset;
+        
+            if (Date.parse('01/01/2011 ' + timeOfCity) < Date.parse('01/01/2011 ' + sunriseTime ) || Date.parse('01/01/2011 ' + timeOfCity) > Date.parse('01/01/2011' + sunsetTime)) {
+                document.body.style.backgroundColor = "darkblue";
+                document.getElementById("weather_result").style.filter = "invert(1)"
+                document.getElementById("title_h1").style.color = "white"
+            } else {
+                document.body.style.backgroundColor = "aqua";
+                document.getElementById("weather_result").style.filter = "invert(0)"
+                document.getElementById("title_h1").style.color = "Black"
+            }
+
+            //console.log("Heure de sunrise : " + data.results.sunrise);
+            console.log("Heure de sunrise : " + sunriseTime); 
+            console.log("Heure de sunset : " + sunsetTime);
+
+        })
+
     })
     .catch(err => console.log(err))
 
@@ -68,7 +98,7 @@ const callbackGetSuccess = function (data) {
             img.style.width = "10%";
         
             switch (true) {
-            case data.daily[i].weather.description == "clear sky" && data.daily[i].clouds == 0:
+            case data.daily[i].weather[0].description == "clear sky":
                 img.src = "./weathers/sun.svg";
                 break;
             case data.daily[i].clouds > 0 && data.daily[i].clouds <= 50:
@@ -88,6 +118,8 @@ const callbackGetSuccess = function (data) {
             document.getElementById("img_result").appendChild(img);
         }
     }
+
+
 //on récupère la valeur saisie lors de la selection d'une option sur le dropdown
 document.addEventListener("DOMContentLoaded", function () {
     // here you can safely manipulate the DOM.
