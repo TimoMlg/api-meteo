a8b42aac00cb1e4f31e7749d55f0bcbef906ee25
TimoMlg <52750239+TimoMlg@users.noreply.github.com>
2022-05-12 00:33:46 +0200

    comments-added

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+/*------------      Fonction d'action       -------------------
+------------    lors de l'appui du bouton      -------------------*/
 
 function btnGetMethod() {
+    callApiOpenCage();
+};
+
+/*------------      Fonctions de call      -------------------
+------------             des API      -------------------*/
+
+const callApiOpenCage = function () {
     const ville = document.getElementById("input-city").value;     //Récupération de la ville depuis la saisie utilisateur
     var lat; //latitude localisation
     var lon; //longitude localisation
-    var current_hour; //heure actuelle de la ville
     const API_KEY = "7218b7f9356a4f16b14e165aa183d933"; //Api key opencage
     let URL = 'https://api.opencagedata.com/geocode/v1/json?q=' + ville + '&key=' + API_KEY + '&language=fr&pretty=1'
 
@@ -18,25 +26,27 @@ function btnGetMethod() {
     .then(data => {
         lat = data.results[0].geometry.lat;
         lon = data.results[0].geometry.lng;
+        //Cette fonction fait elle même appel aux autres fonctions d'api call
         callApiOpenWeather(lat, lon);
         callApiSunriseSunset(lat, lon);
     })
     .catch(err => console.log(err))
-};
+} 
+
+
 //Appel de l'api openweather en passant la latitude et longitude en paramètres. paramètres à récupérer au niveau de la data extraite de l'api openCage
 const callApiOpenWeather = function (lat, lon) {
     const API_KEY = "185f5c86e1d85eed7428b5ba6701f372";
     const url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY +"&units=metric";
     
-    //appel de l'api avec la methode get et appel de la fonction callBackSuccess en cas de succes, sinon error
-    $.get(url, callbackGetSuccess).done()
+    //appel de l'api avec la methode get et appel de la fonction getSuccess en cas de bon call de l'api, sinon error
+    $.get(url, displayWeatherResultsFromOpenWeather).done()
     .fail(function () {
         alert("error")
     })
 }
-/*
-TODO: Terminer le sunrise et sunset (bonus)
-*/
+
+//Appel et récupération des données de l'API sunrise + récupération des données de l'api openWeather pour l'heure
 const callApiSunriseSunset = function(lat, lon) {
 
     const API_KEY = "185f5c86e1d85eed7428b5ba6701f372";
@@ -53,7 +63,21 @@ const callApiSunriseSunset = function(lat, lon) {
     })
     .then(dataSunrise => {
         $.get(urlOpenWeather, function (dataOpenWeather, status) {
-            
+            displayNightAndDayWithTime(dataSunrise, dataOpenWeather);
+        })
+
+    })
+    .catch(err => console.log(err))
+
+}
+
+
+/*------------      Fonctions de traitement      -------------------
+------------    et d'extraction des données des API      -------------------*/
+
+
+//This function get the time and compare it with sunrise time and sunset time of the place and display night or day mode in DOM
+function displayNightAndDayWithTime(dataSunrise, dataOpenWeather) {
             const unixTimeStamp = dataOpenWeather.current.dt; //get timestamp in data
             const dateObject = new Date(unixTimeStamp*1000) //convert to millisecond
             var timeOfCity = dateObject.toLocaleString("en-US", {hour: "numeric", minute: "numeric", second: "numeric"}) // HH:mm:ss AM
@@ -70,19 +94,10 @@ const callApiSunriseSunset = function(lat, lon) {
                 document.getElementById("weather_result").style.filter = "invert(0)"
                 document.getElementById("title_h1").style.color = "Black"
             }
-
-            //console.log("Heure de sunrise : " + data.results.sunrise);
-            console.log("Heure de sunrise : " + sunriseTime); 
-            console.log("Heure de sunset : " + sunsetTime);
-
-        })
-
-    })
-    .catch(err => console.log(err))
-
 }
-//En cas de success de la méthode get, on récupère les données qui nous intéressent
-const callbackGetSuccess = function (data) {
+
+//En cas de success de la méthode get sur l'api Openweather, on récupère et manipule les données qui nous intéressent
+const displayWeatherResultsFromOpenWeather = function (data) {
 
     reloadDivs();
 
@@ -101,7 +116,7 @@ const callbackGetSuccess = function (data) {
             case data.daily[i].weather[0].description == "clear sky":
                 img.src = "./weathers/sun.svg";
                 break;
-            case data.daily[i].clouds > 0 && data.daily[i].clouds <= 50:
+            case data.daily[i].clouds > 0 && data.daily[i].clouds <= 50 && (data.daily[i].weather[0].id <500 || data.daily[i].weather[0].id >531 ):
                 img.src = "./weathers/cloudy.svg";
                 break;
             case data.daily[i].clouds > 50:
@@ -110,14 +125,19 @@ const callbackGetSuccess = function (data) {
             case data.daily[i].snow > 0:
                 img.src = "./weathers/snow.svg";
                 break;
-            default:
+            case data.daily[i].weather[0].id >=500 && data.daily[i].weather[0].id <=531:
                 img.src = "./weathers/rain.svg";
                 break;
         }
             document.getElementById("day").appendChild(dayH3);
             document.getElementById("img_result").appendChild(img);
+            console.log(data.daily[i])
         }
     }
+
+
+/*------------      Fonctions de gestion      -------------------
+--------------    d'évènements de la page      -------------------*/
 
 
 //on récupère la valeur saisie lors de la selection d'une option sur le dropdown
@@ -127,12 +147,14 @@ document.addEventListener("DOMContentLoaded", function () {
         userChoiceDay();
     });
   });
+
 //fonction pour récupérer la valeur entière de la selection utilisateur 
 function userChoiceDay() {
     const selectDays = document.getElementById("nbrDays");
     var value = selectDays.options[selectDays.selectedIndex].value;
     return parseInt(value);
 }
+
  //To reload content of divs img_result and day 
  //We remove child of divs
 function reloadDivs() {
